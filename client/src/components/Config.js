9aba64d83b230f9c68dc046dce75fa7174dffe1e
Yerok <37797937+Yerokkim@users.noreply.github.com>
2020-07-05 23:18:16 +0200

    grid and main

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,8 @@
 //SERVER ROUTES
-export const USER_SERVER = '/api/users';
+export const USER_SERVER = "/api/users";
+
+export const API_URL = "https://api.themoviedb.org/3/";
+
+export const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
+
+export const API_KEY = "235f8d85a7c4198073a406eeb3d67977";
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { FaCode } from "react-icons/fa";
+import { API_URL, API_KEY, IMAGE_BASE_URL } from "../../Config";
+import MainImage from "./Sections/MainImage";
+import GridCards from "../Commons/GridCards";
+import { Row } from "antd";
 
 function LandingPage() {
-    return (
-        <>
-        <div className="app">
-            <FaCode style={{ fontSize: '4rem' }} /><br />
-            <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-        </div>
-        <div style={{ float:'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
-    )
+  const [PMovies, setPMovies] = useState([]);
+  const [PMainMovieImg, setpMainMovieImg] = useState(null);
+  useEffect(() => {
+    const endPoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    fetchMovies(endPoint);
+  }, []);
+
+  const fetchMovies = (endPoint) => {
+    fetch(endPoint)
+      .then((response) => response.json())
+      .then((response) => {
+        setPMovies([...response.results]);
+        setpMainMovieImg(response.results[0]);
+      });
+  };
+
+  console.log(PMainMovieImg, "pmovies");
+  return (
+    <div style={{ width: "100%", margin: "0" }}>
+      {PMainMovieImg && (
+        <MainImage
+          image={`${IMAGE_BASE_URL}w1280${PMainMovieImg.backdrop_path}`}
+          title={PMainMovieImg.original_title}
+          text={PMainMovieImg.overview}
+        />
+      )}
+
+      <div style={{ width: "85%", margin: "1rem auto" }}>
+        <h2>Movie by lates</h2>
+
+        <button>Read More</button>
+
+        <Row gutter={[16, 16]}>
+          {PMovies &&
+            PMovies.map((movies, index) => (
+              <React.Fragment key={index}>
+                <GridCards
+                  image={
+                    movies.poster_path
+                      ? `${IMAGE_BASE_URL}w500${movies.poster_path}`
+                      : null
+                  }
+                  movieId={movies.id}
+                  movieName={movies.original_title}
+                />
+              </React.Fragment>
+            ))}
+        </Row>
+      </div>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
